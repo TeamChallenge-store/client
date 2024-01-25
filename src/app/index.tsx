44b8e4bbfd2ebe307ffc0e18e06737679220506f
Denys Poltorak <itwfrontend@gmail.com>
2024-01-25 09:50:20 +0200

    check deploy (Thu, Jan 25, 2024 09:50:19 AM)

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,9 +1,16 @@
 import { FC } from 'react';
+import shrek from '~shared/images/shrek.gif';
+import './styles.scss';
 
 type TAppProps = object;
 
 const App: FC<TAppProps> = () => {
-  return <h1>Hello, team!!!</h1>;
+  return (
+    <>
+      <h1 className="red">Hello, team!!!</h1>
+      <img src={shrek} alt="shrek" />
+    </>
+  );
 };
 
 export { App };
