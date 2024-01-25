b14e790cbf197479252bc481cd4ea7421777de1c
Denys Poltorak <itwfrontend@gmail.com>
2024-01-25 09:06:21 +0200

    fix: add new linter rules (Thu, Jan 25, 2024 09:06:21 AM)

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -16,5 +16,7 @@ module.exports = {
       'warn',
       { allowConstantExport: true },
     ],
+    'object-curly-newline': ['error', { multiline: true, consistent: true }],
+    'implicit-arrow-linebreak': 'off',
   },
 };
