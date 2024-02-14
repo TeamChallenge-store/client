ea94e7a6a625f0a4077bf91d3e34e2afcdc06acb
Stadnichenko Valeria <stadnicenkovaleri@gmail.com>
2024-02-14 15:24:37 +0200

    feat: counter (Wed, Feb 14, 2024 03:24:35 PM)

--- src/widgets/Header/ui/Icons/Icons.tsx
+++ src/widgets/Header/ui/Icons/Icons.tsx
@@ -1,4 +1,4 @@
-import { FC } from 'react';
+import { FC, useState } from 'react';
 
 import { Icon } from '~shared/ui/Icon';
 
@@ -7,11 +7,16 @@ import css from './Icons.module.scss';
 type TIconsProps = object;
 
 const Icons: FC<TIconsProps> = () => {
+  const [cartCount] = useState(1);
+
   return (
     <div className={css.icons}>
       <Icon className={css.icon} to="/login" type="account" />
       <Icon className={css.icon} type="like" />
-      <Icon className={css.icon} type="cart" />
+      <div className={css.iconBtn}>
+        <Icon className={css.icon} type="cart" />
+        <span className={css.cartCount}>{cartCount}</span>
+      </div>
     </div>
   );
 };
