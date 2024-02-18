95d5951abfe604e42895f5d605ef0ad278dc6132
Denys Poltorak <itwfrontend@gmail.com>
2024-02-18 21:58:19 +0200

    Updates

--- assets/index-Skk6kuc4.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./index-VWuXcgxr.js";const t=()=>e.jsx("div",{children:e.jsx("h1",{children:"Hello team!!!"})});export{t as default};
---
+++ assets/index-wN7s_ogg.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-r7auC9Ah.js";const t=()=>e.jsx("div",{children:e.jsx("h1",{children:"Hello team!!!"})});export{t as default};
