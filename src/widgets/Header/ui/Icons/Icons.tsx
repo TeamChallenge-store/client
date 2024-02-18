c753c34766c6863b74706f5eb3f8ce283fd22277
Denys Poltorak <itwfrontend@gmail.com>
2024-02-18 11:30:30 +0200

    refactor: change header indentations (Sun, Feb 18, 2024 11:30:29 AM)

--- src/widgets/Header/ui/Icons/Icons.tsx
+++ src/widgets/Header/ui/Icons/Icons.tsx
@@ -1,16 +1,14 @@
-import { FC } from 'react';
+import cn from 'classnames';
 
 import { Icon } from '~shared/ui/Icon';
 
 import css from './Icons.module.scss';
 
-type TIconsProps = object;
-
-const Icons: FC<TIconsProps> = () => {
+const Icons = () => {
   return (
     <div className={css.icons}>
-      <Icon className={css.icon} to="/login" type="account" />
-      <Icon className={css.icon} type="like" />
+      <Icon className={cn(css.icon, css.mobHide)} to="/login" type="account" />
+      <Icon className={cn(css.icon, css.mobHide)} type="like" />
       <Icon className={css.icon} type="cart" />
     </div>
   );
