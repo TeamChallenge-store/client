aa07874eab051291611282ecaa384c8c3e55b407
Kateryna Nikolaienko <work.nikolaos@gmail.com>
2024-03-14 19:38:09 +0200

    refactor: change style subscribe block (Thu, Mar 14, 2024 07:38:09 PM)

--- src/shared/ui/SubscribeButton/SubscribeButton.tsx
+++ src/shared/ui/SubscribeButton/SubscribeButton.tsx
@@ -1,17 +1,26 @@
-import { FC } from "react";
+import { FC } from 'react';
 import css from './SubscribeButton.module.scss';
 
 interface ButtonProps {
-  type: "submit" | "reset" | "button" | undefined;
+  type: 'submit' | 'reset' | 'button';
   text: string;
   className?: string;
 }
 
-const SubscribeButton: FC<ButtonProps> = ({ type, text, className }) => {
+const SubscribeButton: FC<ButtonProps> = ({
+  type = 'button',
+  text,
+  className,
+}) => {
   return (
-    <button className={`${css.subscribeButton} ${className || ''}`} type={type}>
-      {text}
-    </button>
+    <div className={css.buttonContainer}>
+      <button
+        className={`${css.subscribeButton} ${className || ''}`}
+        type={type}
+      >
+        {text}
+      </button>
+    </div>
   );
 };
 
