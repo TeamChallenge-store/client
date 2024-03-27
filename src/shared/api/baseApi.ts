3124e2eecfaf93631c440656c8fdcb584d3c97d9
Stadnichenko Valeria <stadnicenkovaleri@gmail.com>
2024-03-27 18:28:20 +0200

    fix: change base api (Wed, Mar 27, 2024 06:28:19 PM)

--- src/shared/api/baseApi.ts
+++ src/shared/api/baseApi.ts
@@ -1,6 +1,6 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
-const BASE_URL = 'http://34.78.48.115:8000/api/v1/';
+const BASE_URL = 'http://34.118.113.97:8000/api/v1/';
 
 const baseApi = createApi({
   reducerPath: 'api',
