25eba2599b91bce640a8c23a3c79da6d84650b99
Kateryna Nikolaienko <work.nikolaos@gmail.com>
2024-03-30 12:36:25 +0200

    refactor: improve code and use CustomButtom for navigation (Sat, Mar 30, 2024 12:36:25 PM)

--- src/widgets/promotional-offers/ui/PromotionalOffers/PromotionalOffers.tsx
+++ src/widgets/promotional-offers/ui/PromotionalOffers/PromotionalOffers.tsx
@@ -4,8 +4,8 @@ import { ProductList } from '~widgets/product-list';
 
 import 'swiper/css';
 
-import css from './PromotionalOffers.module.scss';
 import { CustomButton } from '~shared/ui/CustomButton';
+import css from './PromotionalOffers.module.scss';
 
 const PromotionalOffers = () => {
   const products = [
@@ -70,24 +70,24 @@ const PromotionalOffers = () => {
           <p className={css.offersDescription}>
             Final сlearance on apparel and footwear
           </p>
-          <CustomButton to="/sale" bgColor='white' className={css.offersLink}>
+          <CustomButton to="/sale" bgColor="white" className={css.offersLink}>
             see more
             <svg
-                  width="25"
-                  height="24"
-                  viewBox="0 0 25 24"
-                  fill="none"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    // eslint-disable-next-line max-len
-                    d="M8.64239 6.43312L18.1442 7.34961L17.2277 16.8515M17.4207 7.94582L6.8577 16.6504"
-                    stroke="rgba(17, 29, 19, 1)"
-                    strokeWidth="1.5"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                  />
-                </svg>
+              width="25"
+              height="24"
+              viewBox="0 0 25 24"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                // eslint-disable-next-line max-len
+                d="M8.64239 6.43312L18.1442 7.34961L17.2277 16.8515M17.4207 7.94582L6.8577 16.6504"
+                stroke="rgba(17, 29, 19, 1)"
+                strokeWidth="1.5"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              />
+            </svg>
           </CustomButton>
         </div>
       </div>
@@ -98,7 +98,6 @@ const PromotionalOffers = () => {
           navigation={{
             prevEl: '#swiper-prev',
             nextEl: '#swiper-next',
-            disabledClass: css.swiperArrowDisabled,
           }}
           breakpoints={{
             375: {
@@ -109,17 +108,19 @@ const PromotionalOffers = () => {
             },
             767: {
               slidesPerView: 3,
-              
             },
             1025: {
               slidesPerView: 4,
-            }
+            },
           }}
           modules={[Navigation, Pagination]}
           className={`swiper-wrapper ${css.swiperWrapper}`}
         >
           {products.map(product => (
-            <SwiperSlide key={product.id} className={`${css.swiperSlide} swiper-slide`}>
+            <SwiperSlide
+              key={product.id}
+              className={`${css.swiperSlide} swiper-slide`}
+            >
               <div className={css.imageInner}>
                 <ProductList products={[product]} />
               </div>
@@ -127,44 +128,51 @@ const PromotionalOffers = () => {
           ))}
         </Swiper>
         <div className={css.swiperNavigationArrows}>
-        <div id="swiper-prev" className={`swiper-prev ${css.swiperPrev} `}>
-          <svg
-            width="19.94"
-            height="20.85"
-            viewBox="0 0 22 21"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
+          <CustomButton
+            id="swiper-prev"
+            className={`swiper-prev ${css.swiperPrev} `}
+            bgColor="white"
           >
-            <path
-              // eslint-disable-next-line max-len
-              d="M10.2929 19.0977L1.82227 10.6271L10.2929 2.15648M2.99874 10.6271L20.1752 10.6271"
-              stroke="#111D13"
-              strokeWidth="2"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-            />
-          </svg>
-        </div>
-        <div id="swiper-next" className={`swiper-next ${css.swiperNext}`}>
-          <svg
-            width="19.94"
-            height="20.85"
-            viewBox="0 0 22 21"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
+            <svg
+              width="19.94"
+              height="20.85"
+              viewBox="0 0 22 21"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                // eslint-disable-next-line max-len
+                d="M10.2929 19.0977L1.82227 10.6271L10.2929 2.15648M2.99874 10.6271L20.1752 10.6271"
+                stroke="#111D13"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              />
+            </svg>
+          </CustomButton>
+          <CustomButton
+            id="swiper-next"
+            className={`swiper-next ${css.swiperNext}`}
+            bgColor="white"
           >
-            <path
-              // eslint-disable-next-line max-len
-              d="M10.2929 19.0977L1.82227 10.6271L10.2929 2.15648M2.99874 10.6271L20.1752 10.6271"
-              stroke="#111D13"
-              strokeWidth="2"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-            />
-          </svg>
+            <svg
+              width="19.94"
+              height="20.85"
+              viewBox="0 0 22 21"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                // eslint-disable-next-line max-len
+                d="M10.2929 19.0977L1.82227 10.6271L10.2929 2.15648M2.99874 10.6271L20.1752 10.6271"
+                stroke="#111D13"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              />
+            </svg>
+          </CustomButton>
         </div>
-        </div>
-        
       </div>
     </div>
   );
