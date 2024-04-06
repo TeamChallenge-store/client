c6dd1bc09e950f5a1c9e1f1b7874ec6f7e711201
Kateryna Nikolaienko <work.nikolaos@gmail.com>
2024-04-06 16:20:25 +0300

    feat: add clear button to search input (Sat, Apr 06, 2024 04:20:25 PM)

--- src/features/search/ui/Search/Search.tsx
+++ src/features/search/ui/Search/Search.tsx
@@ -5,6 +5,7 @@ import css from './Search.module.scss';
 
 const Search = () => {
   const [isExpanded, setIsExpanded] = useState(false);
+  const [searchText, setSearchText] = useState('');
 
   const handleFocus = () => {
     setIsExpanded(true);
@@ -12,6 +13,14 @@ const Search = () => {
 
   const handleBlur = () => {
     setIsExpanded(false);
+  };
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchText(event.target.value);
+  };
+
+  const clearSearchText = () => {
+    setSearchText('');
   };
 
   return (
@@ -32,7 +41,16 @@ const Search = () => {
           placeholder="Search.."
           onFocus={handleFocus}
           onBlur={handleBlur}
+          value={searchText}
+          onChange={handleChange}
         />
+        {searchText && (
+          <Icon
+            type="close"
+            onClick={clearSearchText}
+            className={css.clearButton}
+          />
+        )}
       </label>
     </form>
   );
