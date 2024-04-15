c02039140d0fa4de50a744153183a2e3b22e8ed3
Kateryna Nikolaienko <work.nikolaos@gmail.com>
2024-04-15 19:11:42 +0300

    fix: display of product cards in slider (Mon, Apr 15, 2024 07:11:42 PM)

--- src/widgets/promotional-offers/ui/PromotionalOffers/PromotionalOffers.tsx
+++ src/widgets/promotional-offers/ui/PromotionalOffers/PromotionalOffers.tsx
@@ -48,29 +48,26 @@ const PromotionalOffers: FC = () => {
       </div>
       <div className={`${css.offersSlider} container`}>
         <Swiper
-          slidesPerView={4}
-          spaceBetween={15}
+          slidesPerView={1}
+          spaceBetween={24}
           navigation={{
             prevEl: '#swiper-prev',
             nextEl: '#swiper-next',
           }}
           breakpoints={{
-            375: {
-              slidesPerView: 2,
+            0: {
+              slidesPerView: 1,
             },
-            500: {
+            375: {
               slidesPerView: 2,
               spaceBetween: 15,
             },
-            767: {
+            768: {
               slidesPerView: 3,
+              spaceBetween: 20,
             },
             1025: {
               slidesPerView: 4,
-              spaceBetween: 20,
-            },
-            1440: {
-              spaceBetween: 25,
             },
           }}
           modules={[Navigation, Pagination]}
