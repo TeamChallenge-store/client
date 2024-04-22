a4dd4edc8c95273b1b28e7278114b7d60874a8a3
Kateryna Nikolaienko <work.nikolaos@gmail.com>
2024-04-22 20:29:15 +0300

    feat: add adaptive for personalInfo and errorMessage (Mon, Apr 22, 2024 08:29:15 PM)

--- src/shared/ui/InputErrorMessage/InputErrorMessage.tsx
+++ src/shared/ui/InputErrorMessage/InputErrorMessage.tsx
@@ -1,12 +1,18 @@
 import { FC } from 'react';
 import css from './InputErrorMessage.module.scss';
+import warningIcon from '../Icon/icons/warning.svg';
 
 type TInputErrorMessageProps = {
   errorMessage: string;
 };
 
 const InputErrorMessage: FC<TInputErrorMessageProps> = ({ errorMessage }) => {
-  return <div className={css.errorMessage}>{errorMessage}</div>;
+  return (
+    <div className={css.errorMessage}>
+      <img className={css.warningIcon} src={warningIcon} alt="error" />
+      <span className={css.warning}>{errorMessage}</span>
+    </div>
+  );
 };
 
 export { InputErrorMessage };
