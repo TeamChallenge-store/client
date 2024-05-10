825192eff5a8b81b18ce937a281b428da66b7abd
Stadnichenko Valeria <stadnicenkovaleri@gmail.com>
2024-05-10 15:57:01 +0300

    fix: cart endpoint  (Fri, May 10, 2024 03:57:00 PM)

--- src/entities/cart/api/cartApi.ts
+++ src/entities/cart/api/cartApi.ts
@@ -5,20 +5,20 @@ import { IBagProduct, TResponseBag } from '../model/types';
 export const cartApi = baseApi.injectEndpoints({
   endpoints: build => ({
     getCartProduct: build.query<IBagProduct[], void>({
-      query: () => '/cart/',
+      query: () => 'cart',
       keepUnusedDataFor: 30,
       transformResponse: (response: TResponseBag) => response.cart_items,
     }),
     addProductToCart: build.mutation<void, { pk: number; quantity: number }>({
       query: ({ pk, quantity }) => ({
-        url: '/cart/',
+        url: 'cart',
         method: 'POST',
         params: { pk, quantity },
       }),
     }),
     deleteCartProduct: build.mutation<void, { pk: number }>({
       query: ({ pk }) => ({
-        url: '/cart/',
+        url: 'cart',
         method: 'DELETE',
         params: { pk },
       }),
