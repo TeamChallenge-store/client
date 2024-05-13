f392d8d21d3da263ddc34528574ee7884daf1a7f
Stadnichenko Valeria <stadnicenkovaleri@gmail.com>
2024-05-13 14:10:01 +0300

    fix: fix short message style (Mon, May 13, 2024 02:10:00 PM)

--- src/widgets/cart-pop-up/ShortCartMessage/ShortCartMessage.tsx
+++ src/widgets/cart-pop-up/ShortCartMessage/ShortCartMessage.tsx
@@ -1,6 +1,9 @@
 import { useDispatch } from 'react-redux';
 import { toast } from 'react-toastify';
 import { setIsCartOpen } from '~widgets/cart-pop-up/model/slice';
+import doneIcon from './icon/doneIcon.svg';
+
+import css from './ShortCartMessage.module.scss';
 
 const ShortCartMessage = () => {
   const dispatch = useDispatch();
@@ -11,24 +14,33 @@ const ShortCartMessage = () => {
 
   const showMessage = () => {
     toast(
-      <div>
+      <div
+        className={css.cartShortMessage}
+        // style={{ backgroundColor: '#E1ECDF' }}
+      >
+        <div className={css.icon}>
+          <img src={doneIcon} alt="Done Icon" />
+        </div>
         Product added to Cart
         <button
           type="button"
           onClick={handleOpenCart}
-          style={{ marginLeft: '10px' }}
+          className={css.messageLink}
         >
           View
         </button>
       </div>,
       {
         position: 'top-right',
-        autoClose: 5000,
-        hideProgressBar: false,
+        autoClose: false,
+        hideProgressBar: true,
         closeOnClick: true,
         pauseOnHover: true,
         draggable: true,
         progress: undefined,
+        style: {
+          background: '#E1ECDF',
+        },
       },
     );
   };
