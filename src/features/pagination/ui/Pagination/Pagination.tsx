3b0984c51937d2c41e18df606c028a247731fca1
Kateryna Nikolaienko <work.nikolaos@gmail.com>
2024-05-18 12:55:22 +0300

    feat: add hover effect for arrows (Sat, May 18, 2024 12:55:22 PM)

--- src/features/pagination/ui/Pagination/Pagination.tsx
+++ src/features/pagination/ui/Pagination/Pagination.tsx
@@ -17,6 +17,22 @@ const showItems = (currentPage: number) => {
   return 5;
 };
 
+const CustomArrow: FC = () => (
+  <svg
+    width="14"
+    height="24"
+    viewBox="0 0 14 24"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      // eslint-disable-next-line max-len
+      d="M13.0009 1.42151C13.7809 2.20151 13.7809 3.46151 13.0009 4.24151L5.24094 12.0015L13.0009 19.7615C13.7809 20.5415 13.7809 21.8015 13.0009 22.5815C12.2209 23.3615 10.9609 23.3615 10.1809 22.5815L1.00094 13.4015C0.220938 12.6215 0.220938 11.3615 1.00094 10.5815L10.1809 1.40151C10.9409 0.641509 12.2209 0.641509 13.0009 1.42151Z"
+      fill="#2C5030"
+    />
+  </svg>
+);
+
 const Pagination: FC<TPaginationProps> = ({
   handlePageClick,
   totalPages,
@@ -24,12 +40,12 @@ const Pagination: FC<TPaginationProps> = ({
 }) => {
   return (
     <ReactPaginate
-      nextLabel=""
+      nextLabel={<CustomArrow />}
       onPageChange={handlePageClick}
       pageRangeDisplayed={showItems(currentPage)}
       marginPagesDisplayed={0}
       pageCount={totalPages}
-      previousLabel=""
+      previousLabel={<CustomArrow />}
       pageClassName={css.pageItem}
       pageLinkClassName={css.pageLinkItem}
       previousLinkClassName={css.previousArrow}
