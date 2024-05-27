88185a225a4319f3d2a4e91631becd4dd3a66a5c
Stadnichenko Valeria <stadnicenkovaleri@gmail.com>
2024-05-27 21:29:35 +0300

    fix: change footer catalog component (Mon, May 27, 2024 09:29:33 PM)

--- src/widgets/footer/ui/Catalog/Catalog.tsx
+++ src/widgets/footer/ui/Catalog/Catalog.tsx
@@ -1,28 +1,32 @@
 import { FC } from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { setSelectedCategory } from '~widgets/Header/ui/Navbar/model/slice';
+import { navigationData } from './config/navigationData';
 
 import css from './Catalog.module.scss';
 
 const Catalog: FC = () => {
+  const dispatch = useDispatch();
+
+  const handleCategorySelect = (categoryId: string) => {
+    dispatch(setSelectedCategory(categoryId));
+  };
+
   return (
     <article className={css.catalog}>
       <h4 className={css.catalogTitle}>Catalog</h4>
       <ul className={css.catalogList}>
-        <li>
-          <Link to="/catalog/tents">Tents</Link>
-        </li>
-        <li>
-          <Link to="/catalog/backpacks">Clothes</Link>
-        </li>
-        <li>
-          <Link to="/catalog/clothing">Accessories</Link>
-        </li>
-        <li>
-          <Link to="/catalog/shoes">Camping equipment</Link>
-        </li>
-        <li>
-          <Link to="/catalog/shoes">Mountaineering</Link>
-        </li>
+        {navigationData.map(item => (
+          <li key={item.id}>
+            <NavLink
+              to={item.url}
+              onClick={() => handleCategorySelect(item.id)}
+            >
+              {item.title}
+            </NavLink>
+          </li>
+        ))}
       </ul>
     </article>
   );
---
+++ src/widgets/footer/ui/Catalog/config/navigationData.ts
@@ -0,0 +1,27 @@
+export const navigationData = [
+  {
+    id: '1',
+    title: 'Tents',
+    url: 'products/tents',
+  },
+  {
+    id: '2',
+    title: 'Clothes',
+    url: 'products/clothes',
+  },
+  {
+    id: '3',
+    title: 'Accessories',
+    url: 'products/accessories',
+  },
+  {
+    id: '4',
+    title: 'Camping equipment',
+    url: 'products/equipment',
+  },
+  {
+    id: '5',
+    title: 'Mountaineering',
+    url: 'products/mountaineering',
+  },
+];
