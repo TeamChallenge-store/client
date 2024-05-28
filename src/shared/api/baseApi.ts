b47daf837030ef6971bda98f880387c2a5686d7f
Oleksandr Tsybulskyy <154593552+0leksandrr@users.noreply.github.com>
2024-05-28 17:14:36 +0300

    Update baseApi.ts

--- src/shared/api/baseApi.ts
+++ src/shared/api/baseApi.ts
@@ -1,6 +1,6 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
-const BASE_URL = 'http://localhost:8000/api/v1/';
+const BASE_URL = 'http://34.116.206.235:8000/api/v1/';
 
 const baseApi = createApi({
   reducerPath: 'api',
